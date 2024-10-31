885e824d15ea035aab10759fdcff83b0ed610348
Acterion <nikich375@gmail.com>
2024-11-01 00:51:45 +0100

    chore: added missing error <p>

--- users/src/app/views/LoginForm.tsx
+++ users/src/app/views/LoginForm.tsx
@@ -46,7 +46,7 @@ export default function LoginForm() {
             required
           />
         </div>
-
+        {error && <p className="text-red-500 mb-4">{error}</p>}
         <button
           type="submit"
           className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
