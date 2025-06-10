b0a2c3bc472b059498569516facda24dbde25919
Acterion <nikich375@gmail.com>
2025-06-10 15:27:08 +0200

    chore: changed prolific links

--- users/src/app/lib/prolific.ts
+++ users/src/app/lib/prolific.ts
@@ -1,9 +1,9 @@
 export const redirectToProlificFail = () => {
   window.location.href =
-    process.env.PROLIFIC_REJECTION_URL || `https://app.prolific.com/submissions/complete?cc=CLAI1PZS`;
+    process.env.PROLIFIC_REJECTION_URL || `https://app.prolific.com/submissions/complete?cc=CGJXQTHJ`;
 };
 
 export const redirectToProlificComplete = () => {
   window.location.href =
-    process.env.PROLIFIC_COMPLETION_URL || "https://app.prolific.com/submissions/complete?cc=C16GBRWH";
+    process.env.PROLIFIC_COMPLETION_URL || "https://app.prolific.com/submissions/complete?cc=C63MEXCJ";
 };
